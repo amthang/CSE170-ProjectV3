a1e94e5f285a3b4efc70c98d9ad07492c311c6b2
Anjelica Thang <amthang@ucsd.edu>
2016-03-08 08:20:12 +0000

    figured out how to delete and add projects

--- routes/project.js
+++ routes/project.js
@@ -6,6 +6,11 @@ exports.projectInfo = function(req, res) { 
   // query for the specific project and
   // call the following callback
 
+models.Project
+.find(projectID)
+.exec(afterQuery);
+
+
   function afterQuery(err, projects) {
     if(err) console.log(err);
     res.json(projects[0]);
@@ -16,6 +21,24 @@ exports.addProject = function(req, res) {
   var form_data = req.body;
   console.log(form_data);
 
+  var newProj = new models.Project({
+    "title": form_data["project_title"],
+    "date" : form_data["date"],
+    "summary" : form_data["summary"],
+    "image" : form_data["image_url"]
+  });
+
+  newProj.save(finalSave);
+
+  function finalSave(err){
+    if(err) {
+      console.log(err);
+    }
+
+    res.redirect('/');
+    res.send('Add new project works!');
+  }
+
   // make a new Project and save it to the DB
   // YOU MUST send an OK response w/ res.send();
 }
@@ -25,4 +48,11 @@ exports.deleteProject = function(req, res) {
 
   // find the project and remove it
   // YOU MUST send an OK response w/ res.send();
+  models.Project
+  .find({"_id":projectID}).remove(afterQuery);
+
+  function afterQuery(err, projects) {
+    if(err) console.log(err);
+    res.send('Removal works!');
+  }
 }
